da8a21f1a35f1f5edfda8522ad6646d999eb6775
Junior-Stranner <JuniorStranner@gmail.com>
2025-02-13 10:04:54 -0300

    add other project and change the menu icon , improve it

--- Junior-Portfolio/Js/Menu.js
+++ Junior-Portfolio/Js/Menu.js
@@ -1,15 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const menuIcon = document.getElementById('menu-icon');
-    const mobileMenu = document.getElementById('mobile-menu');
-    const closeMenu = document.getElementById('close-menu');
+    const menuIcon = document.getElementById('menu-icon'); 
+    const mobileMenu = document.getElementById('mobile-menu'); 
+    const closeMenu = document.getElementById('close-menu'); 
 
     if (menuIcon && mobileMenu && closeMenu) {
         menuIcon.addEventListener('click', function() {
-            mobileMenu.classList.toggle('active');
+            mobileMenu.classList.add('active'); 
         });
 
         closeMenu.addEventListener('click', function() {
-            mobileMenu.classList.remove('active');
+            mobileMenu.classList.remove('active'); 
         });
     } else {
         console.error("Erro: Um ou mais elementos não foram encontrados no DOM.");
